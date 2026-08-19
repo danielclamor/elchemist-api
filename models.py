46a751f3bf43cd007c8b71fda8cfe0a808aa677a
danielclamor <danielclamor@gmail.com>
2026-08-19 13:59:34 -0600

    fix self.name and change to self.description

--- models.py
+++ models.py
@@ -72,11 +72,11 @@ class Eliquid(Base):
   upc: Mapped[str] = mapped_column(String(12), unique=True, index=True)
   description: Mapped[str] = mapped_column(String(255))
   brand: Mapped[str] = mapped_column(String(255))
-  bottle_color: Mapped[BottleColor] = mapped_column(bottle_color_enum)
   chill_type: Mapped[ChillType] = mapped_column(chill_type_enum)
   nic_type: Mapped[NicType] = mapped_column(nic_type_enum)
   size: Mapped[SizeOption] = mapped_column(size_option_enum)
   nic_level: Mapped[NicLevelOption] = mapped_column(nic_level_option_enum)
+  bottle_color: Mapped[BottleColor] = mapped_column(bottle_color_enum)
   nic_profile_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("nic_profiles.id"), nullable=True)
 
   created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc))
@@ -87,7 +87,7 @@ class Eliquid(Base):
   nic_profile: Mapped["NicProfile"] = relationship()
 
   def __repr__(self) -> str:
-    return f"<Eliquid {self.name!r}>"
+    return f"<Eliquid {self.description!r}>"
 
 
 class Formula(Base):
