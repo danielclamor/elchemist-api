74c24dcf2307635ed909c3a833a29fef20423e5a
danielclamor <danielclamor@gmail.com>
2026-08-21 14:34:21 -0600

    add get_today and generate_production_order_number utility functions

--- api_graphql/resolvers/utils.py
+++ api_graphql/resolvers/utils.py
@@ -1,5 +1,22 @@
+from typing import TYPE_CHECKING
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+if TYPE_CHECKING:
+  from datetime import date
+
 def make_slug(string: str) -> str:
   import re
   tokens = re.sub(r'[^a-zA-Z0-9]', ' ', string).strip().split(' ')
   tokens = [token for token in tokens if token != ""]
   return '-'.join(tokens).lower()
+
+def get_today() -> datetime:
+  import os 
+  from dotenv import load_dotenv
+  load_dotenv()
+  
+  return datetime.now(ZoneInfo(os.getenv("TIMEZONE")))
+
+def generate_production_order_number(date: date, counter: int) -> str:
+  return f"PROD-{date:%Y%m%d}-{counter:06d}"
