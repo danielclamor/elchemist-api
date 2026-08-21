6cff0bcca000cd9cc4a67f8e60579299d9812498
danielclamor <danielclamor@gmail.com>
2026-08-21 14:29:13 -0600

    add production_order resolver

---
+++ api_graphql/resolvers/production_order.py
@@ -0,0 +1,76 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
+import models
+
+from api_graphql.types.enums import FeedbackStatus
+from api_graphql.types.feedback import Feedback
+from api_graphql.types.production_order import (
+  ProductionOrderType,
+  ProductionOrderCreateInput, 
+  ProductionOrderCreatePayload,
+)
+
+from .utils import generate_production_order_number, get_today
+
+# Queries
+def get_all_production_orders(db: Session) -> list[models.ProductionOrder]:
+  return (
+    db.scalars(select(models.ProductionOrder)).unique().all()
+  )
+
+def get_production_order(db: Session, order_number: str) -> models.ProductionOrder:
+  return (
+    db.scalar(select(models.ProductionOrder).where(models.ProductionOrder.order_number == order_number))
+  )
+
+
+# Mutations
+def create_production_order(db: Session, production_order: ProductionOrderCreateInput) -> ProductionOrderCreatePayload:
+  eliquid = db.scalar(select(models.Eliquid).where(models.Eliquid.upc == production_order.eliquid_upc))
+  if not eliquid:
+    return ProductionOrderCreatePayload(
+      production_order=None,
+      feedback=Feedback(
+        status=FeedbackStatus.FAILED,
+        message=f"Eliquid {production_order.eliquid_upc} not found"
+      )
+    )
+  
+  today = get_today().date()
+  counter = db.scalar(
+    select(models.ProductionOrderCounter)
+    .where(models.ProductionOrderCounter.date == today)
+    .with_for_update()
+  )
+
+  if counter:
+    counter.last_number += 1
+  else:
+    counter = models.ProductionOrderCounter(
+      date=today,
+      last_number=1,
+    )
+    db.add(counter)
+
+  db.flush()
+  
+  po_number = generate_production_order_number(date=today, counter=counter.last_number)
+  
+  po = models.ProductionOrder(
+    order_number=po_number,
+    eliquid_id=eliquid.id,
+    quantity=production_order.quantity,
+    is_priority=production_order.is_priority or False
+  )
+  db.add(po)
+  db.commit()
+  db.refresh(po)
+  
+  return ProductionOrderCreatePayload(
+    production_order=ProductionOrderType.from_model(po),
+    feedback=Feedback(
+      status=FeedbackStatus.CANCELLED,
+      message=f"test {po_number}"
+    )
+  )
