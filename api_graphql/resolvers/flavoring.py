6f10e458e9d35bd799e444d7da7ffd62ce636101
danielclamor <danielclamor@gmail.com>
2026-08-21 10:37:52 -0600

    make separate file for flavoring

---
+++ api_graphql/resolvers/flavoring.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+import models
+
+from api_graphql.resolvers.utils import make_slug
+
+from api_graphql.types.flavoring import FlavoringOptionCreateInput
+
+# Queries
+def get_all_flavoring_options(db: Session) -> list[models.FlavoringOption]:
+  return (
+    db.scalars(select(models.FlavoringOption)).all()
+  )
+  
+def get_flavoring_option(db: Session, flavoring_option_slug: str) -> models.FlavoringOption:
+  return (
+    db.scalar(select(models.FlavoringOption).where(models.FlavoringOption.slug == flavoring_option_slug))
+  )
+  
+  
+# Mutations
+def create_flavoring_option(db: Session, flavoring_option: FlavoringOptionCreateInput) -> models.FlavoringOption:
+  flavoring_option_slug = make_slug(flavoring_option.name)
+
+  flavoring_option = models.FlavoringOption(
+    slug=flavoring_option_slug,
+    name=flavoring_option.name,
+    is_vg=flavoring_option.is_vg,
+  )
+
+  db.add(flavoring_option)
+  db.commit()
+  db.refresh(flavoring_option)
+  return flavoring_option
