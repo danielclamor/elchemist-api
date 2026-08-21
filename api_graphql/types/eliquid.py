d24496385f041d496a2b2e5d621c495113aa8e41
danielclamor <danielclamor@gmail.com>
2026-08-21 11:44:17 -0600

    add type checking

--- api_graphql/types/eliquid.py
+++ api_graphql/types/eliquid.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
-from typing import Annotated
+from typing import Annotated, TYPE_CHECKING
 
 import strawberry
 from strawberry import relay
 
 import models
 from api_graphql.types.enums import ChillType, NicType, SizeOption, NicLevelOption, BottleColor
+
+if TYPE_CHECKING:
+  from api_graphql.types.nic_profile import NicProfileType
+  from api_graphql.types.production_order import ProductionOrderType
 
 @strawberry.type
 class EliquidType(relay.Node):
@@ -42,7 +46,7 @@ class EliquidType(relay.Node):
     from api_graphql.types.nic_profile import NicProfileType
     return NicProfileType.from_model(self._model.nic_profile)
   
-  @strawberry.field
+  @relay.connection(relay.ListConnection[Annotated["ProductionOrderType", strawberry.lazy("api_graphql.types.production_order")]])
   def production_orders(self) -> list[Annotated["ProductionOrderType", strawberry.lazy("api_graphql.types.production_order")]]:
     from api_graphql.types.production_order import ProductionOrderType
     return [ProductionOrderType.from_model(o) for o in self._model.production_orders]
