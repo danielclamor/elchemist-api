5bf0c7d1870d0009e9b0bcdbbdd7e35a67aef785
danielclamor <danielclamor@gmail.com>
2026-08-28 12:55:12 -0600

    refactor: change create_flavoring_option return value

--- api_graphql/resolvers/flavoring_option.py
+++ api_graphql/resolvers/flavoring_option.py
@@ -4,7 +4,14 @@ import models
 
 from api_graphql.resolvers.utils import generate_slug
 
-from api_graphql.types.flavoring_option import FlavoringOptionCreateInput
+from api_graphql.types.feedback import Feedback
+from api_graphql.types.enums import FeedbackStatus
+
+from api_graphql.types.flavoring_option import (
+  FlavoringOptionType,
+  FlavoringOptionCreateInput, 
+  FlavoringOptionCreatePayload
+)
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -23,16 +30,34 @@ def get_flavoring_option(db: Session, identifier: "FlavoringOptionIdentifierInpu
   
   
 # Mutations
-def create_flavoring_option(db: Session, flavoring_option: FlavoringOptionCreateInput) -> models.FlavoringOption:
-  flavoring_option_slug = generate_slug(flavoring_option.name)
+def create_flavoring_option(db: Session, input: FlavoringOptionCreateInput) -> FlavoringOptionCreatePayload:
+  slug = generate_slug(input.name)
+  
+  existing = db.scalar(select(models.FlavoringOption).where(models.FlavoringOption.slug == slug))
+  
+  if existing:
+    return FlavoringOptionCreatePayload(
+      flavoring_option=FlavoringOptionType.from_model(existing),
+      feedback=Feedback(
+        status=FeedbackStatus.CANCELLED,
+        message=f"FlavoringOption already exists",
+      )
+    )
 
   flavoring_option = models.FlavoringOption(
-    slug=flavoring_option_slug,
-    name=flavoring_option.name,
-    is_vg=flavoring_option.is_vg,
+    slug=slug,
+    name=input.name,
+    is_vg=input.is_vg,
   )
 
   db.add(flavoring_option)
   db.commit()
   db.refresh(flavoring_option)
-  return flavoring_option
+  
+  return FlavoringOptionCreatePayload(
+    flavoring_option=FlavoringOptionType.from_model(flavoring_option),
+    feedback=Feedback(
+      status=FeedbackStatus.SUCESS,
+      message=None,
+    )
+  )
