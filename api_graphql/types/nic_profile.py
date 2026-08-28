c7e566c6325e8bac915e2420f4c0d92c7bd8f2d8
danielclamor <danielclamor@gmail.com>
2026-08-28 11:40:49 -0600

    refactor: insert Flavoring and rename to NicProfileFlavoring

--- api_graphql/types/nic_profile.py
+++ api_graphql/types/nic_profile.py
@@ -11,7 +11,7 @@ from api_graphql.types.feedback import Feedback
 
 if TYPE_CHECKING:
   from api_graphql.types.formula import FormulaType
-  from api_graphql.types.flavoring import FlavoringType
+  from api_graphql.types.flavoring import FlavoringOptionType
   from api_graphql.types.nic_base import NicBaseType
 
 @strawberry.type
@@ -52,15 +52,14 @@ class NicProfileType(relay.Node):
     from api_graphql.types.formula import FormulaType
     return FormulaType.from_model(self._model.formula)
 
+  @relay.connection(relay.ListConnection["NicProfileFlavoringType"])
+  def flavorings(self) -> list["NicProfileFlavoringType"]:
+    return [NicProfileFlavoringType.from_model(b) for b in self._model.flavorings]
+
   @relay.connection(relay.ListConnection[Annotated["NicBaseType", strawberry.lazy("api_graphql.types.nic_base")]])
   def nic_bases(self) -> list[Annotated["NicBaseType", strawberry.lazy("api_graphql.types.nic_base")]]:
     from api_graphql.types.nic_base import NicBaseType
     return [NicBaseType.from_model(b) for b in self._model.nic_bases]
-
-  @relay.connection(relay.ListConnection[Annotated["FlavoringType", strawberry.lazy("api_graphql.types.flavoring")]])
-  def flavorings(self) -> list[Annotated["FlavoringType", strawberry.lazy("api_graphql.types.flavoring")]]:
-    from api_graphql.types.flavoring import FlavoringType
-    return [FlavoringType.from_model(b) for b in self._model.flavorings]
 
 @strawberry.input
 class NicProfileIdentifierInput:
@@ -164,3 +163,21 @@ class NicProfileUpdateInput:
 class NicProfileUpdatePayload:
   nic_profile: NicProfileType | None
   feedback: Feedback
+  
+@strawberry.type
+class NicProfileFlavoringType:
+  ratio: float
+
+  _model: strawberry.Private[models.Flavoring]
+
+  @classmethod
+  def from_model(cls, f: models.Flavoring) -> "NicProfileFlavoringType":
+    return cls(
+      ratio=f.ratio,
+      _model=f,
+    )
+
+  @strawberry.field
+  def flavoring_option(self) -> Annotated["FlavoringOptionType", strawberry.lazy("api_graphql.types.flavoring")]:
+    from api_graphql.types.flavoring import FlavoringOptionType
+    return FlavoringOptionType.from_model(self._model.flavoring_option)
