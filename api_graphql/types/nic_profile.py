ed8bfa1a236f333262bd9f306ce5898faed8cec9
danielclamor <danielclamor@gmail.com>
2026-09-10 11:31:45 -0600

    feat: add target vg and pg checker

--- api_graphql/types/nic_profile.py
+++ api_graphql/types/nic_profile.py
@@ -144,6 +144,13 @@ class NicProfileCreateInput:
   target_nic_str: float
   target_vg: float
   target_pg: float
+  
+  def __post_init__(self):
+    if sum([self.target_pg, self.target_vg]) != 1:
+      raise GraphQLError(
+        "sum of targetPg and targetVg must be 1",
+        extensions={"code": "INPUT_ERROR", "inputObjectType": self.__strawberry_definition__.name}
+      )
 
 @strawberry.type
 class NicProfileCreatePayload:
